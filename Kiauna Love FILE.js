10ede33824aeb62e8eae5aaf0ba7126e609982eb
Wades94 <wwhiteplume@gmail.com>
2025-02-13 23:43:32 -0700

    Update Kiauna Love FILE.js

--- Kiauna Love FILE.js
+++ Kiauna Love FILE.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Kiauna Love Shongutsie",
+    valentineName: "Kiauna Shongutsie",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dp6e459tp/raw/upload/v1739512931/ze9g3xfb3dycvoy82nr7", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dp6e459tp/video/upload/v1739515036/TV_Girl_-_Lovers_Rock_necllu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
